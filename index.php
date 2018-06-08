407e11aac697206613d5214be8a5d19bc6a3f51a
QQsPB <halobandungkuh@gmail.com>
2018-06-08 15:55:58 -0400

    fix all
    fix

---
+++ index.php
@@ -0,0 +1,183 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//PT-BR" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>PointBlank QQs - Private Server</title>
+<meta http-equiv="content-type" content="text/html;charset=iso-UTF-8">
+<link rel="shortcut icon" href="favicon.ico" />
+<meta name="Description" content="PointBlank: Private Server QQs.">
+<meta name="Abstract" content="PointBlank: Private Server QQs">
+<meta name="Owner" content="deraslah13@gmail.com">
+<meta name="Keywords" content="PointBlank: Private Server QQs">
+
+<script>
+var name = navigator.appName, ver = navigator.appVersion,
+ver_int = parseInt(navigator.appVersion), ua = navigator.userAgent, infostr;
+//alert(ver);
+if(name == "Microsoft Internet Explorer"){
+ if(ver.indexOf("MSIE 9.0") != -1)  {
+  document.getElementById("X-UA-Compatible").setAttribute("content", "IE=9");
+ // document.getElementById("browser_mode").setAttribute("content", "IE=EmulateIE9");
+ }else if(ver.indexOf("MSIE 8.0") != -1)  {
+  document.getElementById("X-UA-Compatible").setAttribute("content", "IE=8;IE8");
+ // document.getElementById("browser_mode").setAttribute("content", "IE=EmulateIE8");
+ }else if(ver.indexOf("MSIE 7.0") != -1)  {
+  document.getElementById("X-UA-Compatible").setAttribute("content", "IE=7");
+ }else{
+  document.getElementById("X-UA-Compatible").setAttribute("content", "IE=edge");
+ }
+}
+</script>
+
+<link rel="stylesheet" type="text/css" href="common/css/seal/common_new.css">
+<link rel="stylesheet" type="text/css" href="common/css/seal/seal_sub_new.css">
+<script src="/code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="common/js/script.js"></script>
+<!--<script type="text/javascript" src="common/js/query-1.9.1.min.js"></script>-->
+<script type="text/javascript" src="common/js/jssor.js"></script>
+<script type="text/javascript" src="common/js/jssor.slider.js"></script>
+<link rel="SHORTCUT ICON" href="images/seal.ico">
+<script type="text/javascript" src="common/js/systemmsg.js"></script>
+<script type="text/javascript" src="common/js/yk.util.js"></script>
+<script type="text/javascript" src="Common/js/default.js"></script>
+<script type="text/javascript" src="Common/js/gamestart.js"></script>
+<script type="text/javascript" src="Common/js/prototype-1-6-0-3.js"></script>
+<script type="text/javascript" src="common/js/https/login/login.js"></script>
+<script type="text/javascript" src="common/js/https/member/member.js"></script>
+<script type="text/javascript" src="common/js/https/member/passcheck.js"></script>
+<script type="text/javascript" language="javascript" src="common/js/googleAnalytics.js"></script>
+</head>
+
+<body>
+
+	
+	<div id="wrapper">
+    	<div class="wrapper">
+        	<header id="header">
+				<!-----------------------------------  Navi -------------------------------------------------------------->
+            	<div class="header_new">
+                	<div class="Seal_logo"><a href="http://qqspb.com"><img src="image/pb_logo.png" /></a></div>
+                    <div class="Menu">
+                      <div id='cssmenu'>
+                        </div>
+                    </div>
+                    <!-----------------------------------  Navi END -------------------------------------------------------------->
+				</div>
+            </header>
+
+
+
+             <!-----------------------------------  Contents Starts  -------------------------------------------------------------->
+            <div class="contents">
+
+            	
+
+<script type="text/javascript" src="promotion/DarkTides/common.js"></script>	
+
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '238744940008078'); 
+fbq('track', 'ViewContent');
+</script>
+<noscript>
+<img height="1" width="1" 
+src="https://www.facebook.com/tr?id=238744940008078&ev=PageView
+&noscript=1">
+</noscript>
+<!-- End Facebook Pixel Code -->
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "/connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=149146458488598";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+<script type="text/javascript" language="javascript" src="common/js/googleAnalytics.js"></script>
+             <!-----------------------------------  Contents Starts  -------------------------------------------------------------->
+           	 <!-----------------------------------  Left Contents Starts  -------------------------------------------------------------->
+            	<div class="left_contentwrapper">
+                	<div class="subtitle" style="text-align:center;">
+                    	<span>Announcements & News</span>
+                    </div>
+                    <div class="sub_content">
+                    	
+					
+
+                        <div class="news_row">
+                            <div class="news_image">
+                               <img src="image/seal_common/news.png" />
+                            </div>
+                            <div class="news_txt">
+                                <div class="news_txt_title">
+									<h1 style="margin-top:0;">
+									QQs Official Info
+									</h1>
+								</div>
+                                Selamat Datang di website baru QQs PointBlank
+                            </div>
+                        </div>
+						<hr style="clear:both;">
+
+						
+                    </div>
+                </div>
+            	<!----------------------------------- Right Sub Menu Starts ----------------------------------------------------------->
+             	<div id='Right_shortcuts'>
+                	<ul>
+                    	<li class="short_li">
+                        	<a href="#">
+                                <div class="short_title">
+                                    Ranking
+                                </div>
+                            </a>
+                        </li>
+                        <li class="short_li">
+                        	<a href='https://drive.google.com/file/d/11zxD5KYXlHex9dlke9mhX74YrNgkqgog/view'>
+                                <div class="short_title">
+                                    Download
+                                </div>
+                            </a>
+                        </li>
+                        <li class="short_li">
+                        	<a href='https://web.facebook.com/groups/246909326044276/'>
+                                <div class="short_title">
+                                    Facebook Groups
+                                </div>
+                            </a>
+                        </li>                        
+                        <li class="short_li">
+                        	<div class="Main_fbWrapper">
+                            	<!--<span>Seal Onlne Facebook</span>-->
+                                
+                                <div class="Main_fbimgs">
+ 
+									<div class="fb-page" data-href="https://www.facebook.com/qqspb" data-width="235" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/qqspb"><a href="#">Facebook</a></blockquote></div></div>
+                                </div>
+                                <hr style="margin: 4px 0; border-top: 1px solid #808080; border-bottom: 1px solid #D4D4D4;" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+            <!-----------------------------------  Contents Ends  -------------------------------------------------------------->
+            <!-----------------------------------  bottom background starts  -------------------------------------------------------------->
+			<!-----------------------------------  bottom background starts  -------------------------------------------------------------->
+			</div>
+		</div>
+
+</body>
+</html>
